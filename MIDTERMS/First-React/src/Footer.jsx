8bb532e98842f541c943c0d9e4b1b04966762655
Amor Pucay <20239228@s.ubaguio.edu>
2024-10-28 11:12:11 +0800

    fixed folders

--- MIDTERMS/First-React/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer(){
-    return(
-        <footer>
-            <p>
-                &copy; {new Date().getFullYear()} My Food Website|| written by Jamie Pucay
-                </p>
-        </footer>
-    )
-}
