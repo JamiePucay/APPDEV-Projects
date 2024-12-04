7933ed3cbf7a23be0b5e5fba740e6a8f26afefd5
Jamie Pucay <20239228@s.ubaguio.edu>
2024-12-04 12:31:22 +0800

    finals

--- FINALS/react-shopping-cart/src/shop.jsx
+++ FINALS/react-shopping-cart/src/shop.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from "react";
 import Login from "./login";
 
@@ -48,54 +47,3 @@ export default function Shop(props) {
     );
   }
 }
-=======
-import { useState } from "react";
-import Login from "./login";
-
-export default function Shop(props) {
-  const ShopItems = props.items;
-  const [cart, setCart] = useState([]);
-
-  function addItem(item) {
-    setCart(c => [...c, item]);
-  }
-
-  const renderItems = () => {
-    return ShopItems.map((item) => (
-      <div key={item.id}>
-        <li>{item.name}</li>
-        <p>Price per piece: ${item.price}</p>
-        <button onClick={() => addItem(item)}>Add to Cart</button>
-      </div>
-    ));
-  };
-
-  const renderCart = () => {
-    return cart.map((item) => (
-      <div key={item.id}>
-        <li>{item.name}</li>
-        <p>Price per piece: ${item.price}</p>
-        <br />
-      </div>
-    ));
-  };
-
-  if (!props.isLoggedIn) {
-    return <Login items={props.items} />;
-  } else {
-    return (
-      <>
-        <h4>This is the shop: </h4>
-        <ul>{renderItems()}</ul>
-
-        <h4>Cart Items: </h4>
-        {cart.length > 0 ? (
-          <ul>{renderCart()}</ul>
-        ) : (
-          <p>There are no items in the cart!</p>
-        )}
-      </>
-    );
-  }
-}
->>>>>>> 5d0e1034a9ec9a023f9ea8375d84e1bd5e95f81b
